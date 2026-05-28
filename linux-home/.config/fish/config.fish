77afe4919dcf7e0a2ce5e515bd90c61482329ac2
Håkon Skogsrud <hakon.skogsrud@gjensidige.no>
2026-05-28 10:01:31 +0200

    updates

--- linux-home/.config/fish/config.fish
+++ linux-home/.config/fish/config.fish
@@ -1,7 +1,36 @@
-source ~/.config/fish/cachyos-config.fish
+# Format man pages
+set -x MANROFFOPT "-c"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+fish_add_path ~/.local/bin ~/.cargo/bin
+
+# eza (modern ls)
+alias ls='eza -al --color=always --group-directories-first --icons=always'
+alias la='eza -a --color=always --group-directories-first --icons=always'
+alias ll='eza -l --color=always --group-directories-first --icons=always'
+alias lt='eza -aT --color=always --group-directories-first --icons=always'
+alias l.="eza -a | grep -e '^\.'"
+
+# Common utils
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+
+## Shell tools
 zoxide init fish | source
 fzf --fish | source
 complete -c ansible-playbook -e
+
+# fd as fzf backend
+alias find="fd"
+set -x FZF_DEFAULT_COMMAND "fd --type f --hidden --exclude .git"
+set -x FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -x FZF_ALT_C_COMMAND "fd --type d --hidden --exclude .git"
+set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border --preview 'bat --color=always --line-range :50 {}' --preview-window=right:50%:wrap"
 
 function fish_greeting
     # smth smth
