e447c842477cf72ff5c65f9f8989076f75e33bb2
Håkon Skogsrud <haakon.skogsrud@pm.me>
2026-08-04 19:48:12 +0200

    updates

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -5,17 +5,22 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-flatpak = {
       url = "github:gmodena/nix-flatpak/?ref=v0.7.0";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nix-flatpak }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        nix-flatpak.nixosModules.nix-flatpak
-        ./configuration.nix
-      ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nix-flatpak,
+    }:
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nix-flatpak.nixosModules.nix-flatpak
+          ./configuration.nix
+        ];
+      };
     };
-  };
 }
